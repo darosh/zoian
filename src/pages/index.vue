072f7322f02d4dd0a151c1a1adfe646c3d4ba24f
Jan Forst <ensonador@gmail.com>
2024-12-22 00:28:39 +0100

    font fix

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -585,10 +585,10 @@ export default {
 }
 </style>
 
-<style lang="scss">
+<style>
 /* noto-sans-latin-wght-normal */
 @font-face {
-  font-family: 'Noto Sans Variable';
+  font-family: 'Noto Sans';
   font-style: normal;
   font-display: swap;
   font-weight: 100 900;
@@ -598,14 +598,16 @@ export default {
 
 /* montserrat-latin-wght-normal */
 @font-face {
-  font-family: 'Montserrat Variable';
+  font-family: 'Montserrat';
   font-style: normal;
   font-display: swap;
   font-weight: 100 900;
   src: url(https://cdn.jsdelivr.net/fontsource/fonts/montserrat:vf@latest/latin-wght-normal.woff2) format('woff2-variations');
   unicode-range: U+0000-00FF,U+0131,U+0152-0153,U+02BB-02BC,U+02C6,U+02DA,U+02DC,U+0304,U+0308,U+0329,U+2000-206F,U+2074,U+20AC,U+2122,U+2191,U+2193,U+2212,U+2215,U+FEFF,U+FFFD;
 }
+</style>
 
+<style lang="scss">
 * {
   font-family: "Noto Sans", sans-serif !important;
 }

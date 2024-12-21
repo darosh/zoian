053d05434db7e4d1f3fc2e1a596c1e30fc15e1cc
Jan Forst <ensonador@gmail.com>
2024-12-21 15:31:03 +0100

    adding config files

---
+++ eslint.config.js
@@ -0,0 +1,49 @@
+/**
+ * .eslint.js
+ *
+ * ESLint configuration file.
+ */
+
+import pluginVue from 'eslint-plugin-vue'
+import vueTsEslintConfig from '@vue/eslint-config-typescript'
+
+export default [
+  {
+    name: 'app/files-to-lint',
+    files: ['**/*.{ts,mts,tsx,vue}'],
+  },
+
+  {
+    name: 'app/files-to-ignore',
+    ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
+  },
+
+  ...pluginVue.configs['flat/recommended'],
+  ...vueTsEslintConfig(),
+
+  {
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-expressions': [
+        'error',
+        {
+          allowShortCircuit: true,
+          allowTernary: true,
+        },
+      ],
+      'vue/multi-word-component-names': 'off',
+      'vue/block-lang': 'off',
+      'vue/html-closing-bracket-newline': [
+        'error',
+        {
+          'singleline': 'never',
+          'multiline': 'never',
+          'selfClosingTag': {
+            'singleline': 'never',
+            'multiline': 'never'
+          }
+        }
+      ]
+    }
+  }
+]

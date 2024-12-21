e31950656493d57afc8e80540d0dd4f5dbfd27f1
Jan Forst <ensonador@gmail.com>
2024-12-21 15:58:26 +0100

    adding stores

---
+++ src/stores/app.ts
@@ -0,0 +1,16 @@
+import { defineStore } from "pinia";
+import { useLocalStorage } from "@vueuse/core";
+
+// You can name the return value of `defineStore()` anything you want,
+// but it's best to use the name of the store and surround it with `use`
+// and `Store` (e.g. `useUserStore`, `useCartStore`, `useProductStore`)
+// the first argument is a unique id of the store across your application
+export const useAppStore = defineStore("app", {
+  state: () => ({
+    dark: useLocalStorage("dark", false),
+    columns: useLocalStorage("columns", 4),
+    zebu: useLocalStorage("zebu", true),
+    showConnections: useLocalStorage("showConnections", true),
+    animations: useLocalStorage("animations", false),
+  }),
+});

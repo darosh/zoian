7d0760e4a9dc35d3930ac8eac3e80ba53d5d7401
Jan Forst <ensonador@gmail.com>
2024-12-31 13:46:19 +0100

    param parsing raw + order of params fix

--- lib/parser/parse-parameters.ts
+++ lib/parser/parse-parameters.ts
@@ -6,7 +6,8 @@ export function parseParameters(longs: Int32Array, cursor: number): Record<strin
   const numParams = longs[cursor + 6]
 
   for (let i = 0; i < numParams; i++) {
-    params[`param_${i}`] = Math.round(longs[cursor + i + 10] / 65535 * 100) / 100
+    // params[`param_${i}`] = Math.round(longs[cursor + i + 10] / 65535 * 100) / 100
+    params[`param_${i}`] = longs[cursor + i + 10]
   }
 
   return params
@@ -15,18 +16,14 @@ export function parseParameters(longs: Int32Array, cursor: number): Record<strin
 export function renameParamDict(module: PatchModule): void {
   const paramBlocks = Object.entries(module.blocks)
     .filter(([, block]: [string, Block]) => block.param)
-    .reduce((acc: { [key: string]: boolean }, [key]) => {
-      acc[key] = true
-      return acc
-    }, {})
+    .sort((a, b) => a[1].position - b[1].position)
 
-  const paramNames = Object.keys(paramBlocks)
-
-  for (let i = 0; i < paramNames.length; i++) {
+  for (let i = 0; i < paramBlocks.length; i++) {
+    // const oldKey = `param_${paramBlocks[i][1].position}`
     const oldKey = `param_${i}`
 
     if (module.parameters[oldKey] !== undefined) {
-      module.parameters[paramNames[i]] = module.parameters[oldKey]
+      module.parameters[paramBlocks[i][0]] = module.parameters[oldKey]
       // log('moving param: %s to %s', oldKey, paramNames[i])
 
       delete module.parameters[oldKey]

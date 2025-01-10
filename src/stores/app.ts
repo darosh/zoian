b752516b819b54f4c5f91a083259bdc4570c86a8
Jan Forst <ensonador@gmail.com>
2025-01-10 20:17:29 +0100

    show parameters

--- src/stores/app.ts
+++ src/stores/app.ts
@@ -1,6 +1,11 @@
 import { defineStore } from 'pinia'
 import { useLocalStorage } from '@vueuse/core'
 
+if (!localStorage.version) {
+  localStorage.version = '1'
+  localStorage.showParameters = 'true'
+}
+
 // You can name the return value of `defineStore()` anything you want,
 // but it's best to use the name of the store and surround it with `use`
 // and `Store` (e.g. `useUserStore`, `useCartStore`, `useProductStore`)
